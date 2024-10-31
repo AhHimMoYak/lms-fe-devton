a1b8ac458992d6626df249631bf195705906f587
tiktokyi <tiktokyi@gmail.com>
2024-10-31 20:22:31 +0900

    new : 'npm install react-icons' 필요, components/NoneUserMenu.jsx에 Header 아이콘 추가, styles/Main/LoginComponent.css에 폰트 사이즈 변경

--- src/components/NoneUserMenu.jsx
+++ src/components/NoneUserMenu.jsx
@@ -1,22 +1,31 @@
 // HeaderButtons.jsx
 import { useNavigate } from "react-router-dom";
+import { CiLogin } from "react-icons/ci";
+import { AiOutlineUserAdd } from "react-icons/ai";
 
 function NoneUserMenu() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleCoursesClick = () => navigate("/course");
-    const handleSignInClick = () => navigate("/signin");
-    const handleSignUpClick = () => navigate("/signup");
+  const handleCoursesClick = () => navigate("/course");
+  const handleSignInClick = () => navigate("/signin");
+  const handleSignUpClick = () => navigate("/signup");
 
-    return (
-        <div className="header-buttons">
-            <button className="go-course" onClick={handleCoursesClick}>강의</button>
-            <div className="auth-buttons">
-                <button onClick={handleSignInClick}>Sign In</button>
-                <button onClick={handleSignUpClick}>Sign Up</button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="header-buttons">
+      <button className="go-course" onClick={handleCoursesClick}>
+        강의
+      </button>
+      <div className="auth-buttons">
+        {/* <button onClick={handleSignInClick}>Sign In</button> */}
+        <button className="login-button" onClick={handleSignInClick}>
+          <CiLogin /> 로그인
+        </button>
+        <button className="join-button" onClick={handleSignUpClick}>
+          <AiOutlineUserAdd /> 회원가입
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default NoneUserMenu;
