8500b71e6a968537c662ceb2df89e2637542ea7e
sora0319 <sojeoung536985@gmail.com>
2024-11-19 18:58:48 +0900

    feat : live stream일 때 footer 삭제

--- src/components/BasicFrame.jsx
+++ src/components/BasicFrame.jsx
@@ -1,13 +1,18 @@
-import { Outlet } from "react-router-dom";
-import Footer from "./Footer";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "./Header";
+import Footer from "./Footer";
 
 function BasicFrame() {
+    const location = useLocation();
+
+    // 특정 경로에서 Footer를 숨기기 (정규식으로 경로 확인)
+    const hideFooter = /^\/live\/[^/]+$/.test(location.pathname);
+
     return (
         <>
             <Header />
             <Outlet />
-            <Footer />
+            {!hideFooter && <Footer />}
         </>
     );
 }
